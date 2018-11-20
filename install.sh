26a3851a52ba5efa21059e08705efd6af919f1a8
Stefan Benten <mail@stefan-benten.de>
2018-11-20 17:16:21 +0100

    Initial Commit
    Install Script for getting a Raiden Node

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+RAIDENV=v0.17.0
+RAIDENBIN=raiden-$RAIDENV-linux.tar.gz
+
+echo "Fetching Onboarder Tool"
+curl -O https://raiden-nightlies.ams3.digitaloceanspaces.com/onboarder-linux.tar.gz
+tar -xzvf onboarder-linux.tar.gz
+rm onboarder-linux.tar.gz
+echo "Fetching Raiden $RAIDENV"
+curl -O https://raiden-nightlies.ams3.digitaloceanspaces.com/$RAIDENBIN
+tar -xzvf $RAIDENBIN
+rm $RAIDENBIN
