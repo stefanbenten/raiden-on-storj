d5bb10d18e66f822b3fa6db7b93acc9e194470e0
Shawn Wilkinson <me@super3.org>
2018-12-03 13:35:53 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ./raiden-v0.17.0-linux \
     --keystore-path keystore \
-    --password YOURSECRETPASSWORD \
     --network-id kovan \
     --environment-type development \
     --gas-price 20000000000 \
-    --eth-rpc-endpoint http://localhost:8545 \
+    --eth-rpc-endpoint https://kovan.infura.io/v3/d636d4118e2f4cab9b72296d74b9ff62 \
     --api-address 0.0.0.0:8888
+    
