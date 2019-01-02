84629abc4e09703cb49a68c21c55f21493308833
stefanbenten <mail@stefan-benten.de>
2019-01-02 14:19:28 +0100

    Improve Close Channel Logging

--- satellite/main.go
+++ satellite/main.go
@@ -135,13 +135,19 @@ func closeChannel(receiver string) (err error) {
 	var jsonr map[string]interface{}
 	message := `{"state": "closed"}`
 	status, body, err := raidenlib.SendRequest("PATCH", raidenEndpoint+"channels", message, "application/json")
+	if err != nil {
+		return err
+	}
 	if status == http.StatusOK {
 		err = json.Unmarshal([]byte(body), &jsonr)
 		if err != nil && jsonr["state"] != "closed" && jsonr["partner_address"] == receiver {
+			log.Println(jsonr)
 			return errors.New("unable to close channel! Please check the Raiden log files")
 		}
+		return nil
+	} else {
+		return errors.New("unable to close channel! Please check the Raiden log files")
 	}
-	return
 }
 
 func checkChannel(receiver string) (id int64, err error) {
